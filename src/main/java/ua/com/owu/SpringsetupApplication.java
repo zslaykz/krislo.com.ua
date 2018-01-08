6647c18403c090237e35389cb80a44c118fa69a7
zslaykz <zslaykz@gmail.com>
2018-01-08 20:54:46 +0200

    SpringBootSetupProject

---
+++ src/main/java/ua/com/owu/SpringsetupApplication.java
@@ -0,0 +1,12 @@
+package ua.com.owu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsetupApplication.class, args);
+	}
+}
